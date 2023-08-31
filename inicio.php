bd9100d4a247c1efe1b982705bafc730c3975143
majoalt <maxialtamiranda3@gmail.com>
2023-08-31 11:59:56 -0300

    se agrego un parrafo al archivo inicio

--- inicio.php
+++ inicio.php
@@ -6,6 +6,12 @@
     <title>Document</title>
 </head>
 <body>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero assumenda praesentium
+        
+    
+    aperiam voluptatem numquam corrupti rem consectetur perspiciatis perferendis! At assumenda
+    
+    blanditiis illo itaque quo officia voluptates esse nulla ipsam.</p>
     
 </body>
 </html>
